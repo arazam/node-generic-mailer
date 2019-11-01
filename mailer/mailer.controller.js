7b17aecc0d099bc4d5c5753cd3c816438adb05e7
arazam18 <55618618+arazam18@users.noreply.github.com>
2019-11-01 19:44:15 +0530

    Update mailer.controller.js

--- mailer/mailer.controller.js
+++ mailer/mailer.controller.js
@@ -20,9 +20,9 @@ async function mail(req, res, next) {
         //   user: "app@trueverifier.com", // generated ethereal user
         //   pass: "#5=K~2_5TUPo" // generated ethereal password
         // host : "smtp.zoho.in" host : "smtp.gmail.com",}
-        host : "smtp.usedpartsus.com",
+        host : "mail.usedpartsus.com",
         port: 465,
-        secure: false, // true for 465, false for other ports
+        secure: true, // true for 465, false for other ports
         auth: {
          // user: "fredsmith.1376@gmail.com", // generated ethereal user
          // pass: "GjsAUnXCK5vt" // generated ethereal password
